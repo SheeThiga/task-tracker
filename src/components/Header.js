f5ba2adc59a25ac4acc27d35e4325a228cceb96a
SheeThiga <thigamercy399@gmail.com>
2024-03-26 15:46:14 +0300

    chore: integrated the mock API and lcation.pathname to enable/disable the button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import Button from "./Button";
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "close" : "add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "close" : "add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
